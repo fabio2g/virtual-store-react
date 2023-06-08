105f2f6b864406de2f615c73170a8dc08f33d81e
fabiodev88 <fabio_dev@outlook.com>
2023-06-08 14:34:05 -0300

    corrigindo erros de conexão com a base de dados

--- backend/App.js
+++ backend/App.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
@@ -11,6 +12,11 @@ app.use(express.urlencoded({ extended: true }));
  * Arquivo de rotas
  */
 const router = require("./routes/Routes");
+
+require("./config/database");
+
 app.use(router);
 
-app.listen(3000);
+app.listen(process.env.PORT || 8080, () => {
+    console.log("[app] servidor rodando");
+});
--- backend/config/database.js
+++ backend/config/database.js
@@ -1,20 +1,21 @@
 const mongoose = require("mongoose");
 
-const userDB = process.env.DATABASE_USER;
-const passDB = process.env.DATABASE_PASSWORD;
+const userDB = process.env.DB_USER;
+const passDB = process.env.DB_PASSWORD;
 
 mongoose.set("strictQuery", false);
 
 const conn = async () => {
     try {
-        const connectionDB = await mongoose.connect(
+        const dbConn = await mongoose.connect(
             `mongodb+srv://${userDB}:${passDB}@cluster0.gaxnhat.mongodb.net/?retryWrites=true&w=majority`
         );
-        console.log("[mongoose] base de dados conectada");
 
-        return connectionDB;
+        console.log(`[mongoose] base de dados conectada`);
+
+        return dbConn;
     } catch (error) {
-        console.log("Erro ao conectar o banco de dados");
+        console.log("Erro ao conectar o banco!");
         console.log(error);
     }
 };
@@ -22,5 +23,3 @@ const conn = async () => {
 conn();
 
 module.exports = conn;
-
-// mongodb+srv://fabio2g:<password>@cluster0.gaxnhat.mongodb.net/?retryWrites=true&w=majority
