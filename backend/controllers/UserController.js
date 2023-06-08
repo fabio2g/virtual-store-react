41c5629a6ba8747edd396e4ed4a3f8b8ffa7b259
fabio2g <fabiodev88@gmail.com>
2023-06-08 17:41:29 -0300

    criado o modelo de usuário

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,14 +2,14 @@
  * Realiza o registro do usuário no banco de dados
  */
 const register = async (req, res) => {
-    await res.status(201).json({ message: "Registro feito com sucesso" });
+    await res.status(201).json({ message: "Usuário registrado com sucesso." });
 };
 
 /**
  * Realiza a validação de login do usuário
  */
 const login = async (req, res) => {
-    await res.status(201).json({ message: "Login realizado com sucesso" });
+    await res.status(201).json({ message: "Login realizado com sucesso." });
 };
 
 module.exports = {
