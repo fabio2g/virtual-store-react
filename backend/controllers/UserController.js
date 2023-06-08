aad445e21144c05fd97b2379635d58f79a2abf07
fabio2g <fabiodev88@gmail.com>
2023-06-08 18:23:55 -0300

    criado o registro do usuário no banco de dados

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,12 +1,48 @@
-/**
- * Realiza o registro do usuário no banco de dados
- */
-const register = async (req, res) => {
-    await res.status(201).json({ message: "Usuário registrado com sucesso." });
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+const generatedToken = (id) => {
+    return id;
 };
 
 /**
- * Realiza a validação de login do usuário
+ * - Rota de registro
+ * - Realiza o registro do usuário no banco de dados
+ */
+const register = async (req, res) => {
+    const { name, email, password } = req.body;
+
+    const checkEmail = await User.findOne({ email });
+
+    if (checkEmail) {
+        res.status(422).json({ error: ["Por favor, utilize outro e-mail."] });
+        return;
+    }
+
+    // criptografia da senha
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = await User.create({
+        name,
+        email,
+        password: passwordHash,
+    });
+
+    if (!newUser) {
+        res.status(422).json({ error: ["Erro ao criar usuário."] });
+        return;
+    }
+
+    res.status(201).json({
+        _id: newUser._id,
+        token: generatedToken(newUser._id),
+    });
+};
+
+/**
+ * - Rota de login
+ * - Realiza a validação de login do usuário
  */
 const login = async (req, res) => {
     await res.status(201).json({ message: "Login realizado com sucesso." });
