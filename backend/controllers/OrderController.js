e1e60bab426ab649aad21898b6ecc139e4b8bf11
fabio2g <fabiodev88@gmail.com>
2023-07-22 16:49:26 -0300

    refatorado o model Order

--- backend/controllers/OrderController.js
+++ backend/controllers/OrderController.js
@@ -1,31 +1,41 @@
 const Order = require("../models/Order");
 const Product = require("../models/Product");
 
-const calcTotalPrice = (obj) => {};
-
 const createOrder = async (req, res) => {
-    const user = req.user._id;
+    try {
+        const userId = req.user._id;
+        const products = req.body;
 
-    const products = req.body;
+        if (!products || !Array.isArray(products) || products.length === 0) {
+            return res
+                .status(400)
+                .json({ error: "Products array is missing or empty." });
+        }
 
-    if (!products) {
-        return;
+        let totalPrice = 0;
+
+        for (let product of products) {
+            const item = await Product.findById(product.productId);
+
+            if (!item) {
+                return res.status(404).json({ error: "Product not found." });
+            }
+
+            product.unitPrice = item.price;
+            product.totalPrice = product.quantity * product.unitPrice;
+            totalPrice += product.totalPrice;
+        }
+
+        const newOrder = await Order.create({
+            userId,
+            products,
+            totalPrice,
+        });
+
+        res.status(201).json(newOrder);
+    } catch (err) {
+        res.status(500).json({ error: "Internal server error." });
     }
-
-    for (let product of products) {
-        let item = await Product.findById(product.productId);
-
-        product.unitPrice = item.price;
-        product.totalPrice = product.quantity * product.unitPrice;
-    }
-
-    // const newOrder = await Order.create({
-    //     user,
-    //     products,
-    //     totalPrice,
-    // });
-
-    res.status(201).json(user);
 };
 
 module.exports = createOrder;
--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,32 +1,35 @@
 const mongoose = require("mongoose");
 
+const productSchema = new mongoose.Schema(
+    {
+        productId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Product",
+            required: true,
+        },
+        quantity: {
+            type: Number,
+            required: true,
+        },
+        unitPrice: {
+            type: Number,
+            required: true,
+        },
+        totalPrice: {
+            type: Number,
+            required: true,
+        },
+    },
+    { _id: false }
+);
+
 const orderSchema = new mongoose.Schema({
-    user: {
+    userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
         required: true,
     },
-    products: [
-        {
-            productId: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "Product",
-                required: true,
-            },
-            quantity: {
-                type: Number,
-                required: true,
-            },
-            unitPrice: {
-                type: Number,
-                required: true,
-            },
-            totalPrice: {
-                type: Number,
-                required: true,
-            },
-        },
-    ],
+    products: [productSchema],
     totalPrice: {
         type: Number,
         required: true,
