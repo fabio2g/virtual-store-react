c2756364f217a053fd6bfc419f126b3a17726a9c
fabio2g <fabiodev88@gmail.com>
2023-08-06 09:45:44 -0300

    comentando o código

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,34 +2,43 @@ const User = require("../models/User");
 const Product = require("../models/Product");
 const UserService = require("../services/UserService");
 
+/**
+ * Função assíncrona responsável por realizar o processo de cadastro (signup) de um novo usuário.
+ * @param {Object} req - O objeto de requisição contendo os dados enviados pelo cliente.
+ * @param {Object} res - O objeto de resposta que será enviado ao cliente.
+ */
 const signUp = async (req, res) => {
     const { name, email, password } = req.body;
 
+    // Chama o serviço UserService.save() para salvar o novo usuário no banco de dados.
     const newUser = await UserService.save({
         name,
         email,
         password,
     });
 
-    /**
-     * Verifica o status retornado do método save().
-     * Caso o retorno seja false, a resposta HTTP será configurada com o status 401 (Unauthorized)
-     * e o objeto newUser será enviado como resposta JSON.
-     * Caso contrário, a resposta HTTP será configurada com o status 200 (OK - Sucesso)
-     * e o objeto newUser será enviado como resposta JSON.
-     */
+    // Verifica se o status do objeto newUser é false (indicando erro no salvamento do usuário).
+    // Se o status do objeto newUser for true (indicando sucesso no salvamento do usuário).
     if (!newUser.status) return res.status(401).json(newUser);
     res.status(200).json(newUser);
 };
 
+/**
+ * Função assíncrona responsável por realizar o processo de autenticação (login) do usuário.
+ * @param {Object} req - O objeto de requisição contendo os dados enviados pelo cliente.
+ * @param {Object} res - O objeto de resposta que será enviado ao cliente.
+ */
 const signIn = async (req, res) => {
     const { email, password } = req.body;
 
+    // Chama o serviço UserService.login() para realizar o processo de autenticação do usuário.
     const user = await UserService.login({
         email,
         password,
     });
 
+    // Verifica se o status do objeto user é false (indicando falha na autenticação).
+    // Se o status do objeto user for true (indicando sucesso na autenticação),
     if (!user.status) return res.status(401).json(user);
     res.status(200).json(user);
 };
