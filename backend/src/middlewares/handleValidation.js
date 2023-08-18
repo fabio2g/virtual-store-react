e2eb3556a2dc984e75f484a8961967da3d64400b
fabio2g <fabiodev88@gmail.com>
2023-08-17 21:14:58 -0300

    removendo comentários

--- backend/src/middlewares/handleValidation.js
+++ backend/src/middlewares/handleValidation.js
@@ -4,16 +4,7 @@ const validate = (err, req, res, next) => {
     if (err) {
         return res.status(500).json({ status: false, error: err });
     }
-
     next();
-
-    // const errors = validationResult(req);
-    // if (errors.isEmpty()) {
-    //     return next();
-    // }
-    // const extractedErros = [];
-    // errors.array().map((err) => extractedErros.push(err.msg));
-    // return res.status(422).json({ success: false, error: extractedErros });
 };
 
 module.exports = validate;
