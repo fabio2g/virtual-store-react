7dd93eafc060db78c50b19fefb61999e7f1e5036
Fabio <fabio2gx@gmail.com>
2023-08-22 22:48:43 -0300

    Atualizar o App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
     return (
         <div className="App">
-            <h1>Loja Virtual On-line</h1>
+            <h1>Loja Virtual</h1>
         </div>
     );
 }
